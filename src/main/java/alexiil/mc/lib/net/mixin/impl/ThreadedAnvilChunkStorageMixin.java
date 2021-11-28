541df6a7919e23b196e9e0709df2e54d966631ae
AlexIIL <alexj9@me.com>
2021-11-28 14:22:58 +0000

    Theroetically fix mixin.

--- src/main/java/alexiil/mc/lib/net/mixin/impl/ThreadedAnvilChunkStorageMixin.java
+++ src/main/java/alexiil/mc/lib/net/mixin/impl/ThreadedAnvilChunkStorageMixin.java
@@ -7,26 +7,27 @@
  */
 package alexiil.mc.lib.net.mixin.impl;
 
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.network.Packet;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import net.minecraft.world.chunk.WorldChunk;
 
 import alexiil.mc.lib.net.impl.BlockEntityInitialData;
-import alexiil.mc.lib.net.mixin.api.IBlockEntityInitialData;
 
 @Mixin(ThreadedAnvilChunkStorage.class)
 public abstract class ThreadedAnvilChunkStorageMixin {
 
     @Inject(at = @At("RETURN"), method = "sendChunkDataPackets(Lnet/minecraft/server/network/ServerPlayerEntity;"
-        + "[Lnet/minecraft/network/Packet;Lnet/minecraft/world/chunk/WorldChunk;)V")
-    private void postSendPackets(ServerPlayerEntity player, Packet<?>[] packets, WorldChunk chunk, CallbackInfo ci) {
+        + "Lorg/apache/commons/lang3/mutable/MutableObject;Lnet/minecraft/world/chunk/WorldChunk;)V")
+    private void postSendPackets(
+        ServerPlayerEntity player, MutableObject<?> packet, WorldChunk chunk, CallbackInfo ci
+    ) {
         for (BlockEntity be : chunk.getBlockEntities().values()) {
             if (be instanceof BlockEntityInitialData) {
                 ((BlockEntityInitialData) be).sendInitialData(player);
